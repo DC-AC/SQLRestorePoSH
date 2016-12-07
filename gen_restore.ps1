fa234d9fcba44221e9c400945597e368c88f98a9
jdanton <jdanton1@yahoo.com>
2016-12-07 09:10:59 -0600

    Update gen_restore.ps1

--- gen_restore.ps1
+++ gen_restore.ps1
@@ -1,15 +1,27 @@
-﻿$Source = read-host -Prompt 'What is source SQL Server for backups?'
+$Source = read-host -Prompt 'What is source SQL Server for backups?'
 $target = read-host -Prompt 'What is target SQL Server for restores?'
 
 $1=Invoke-SQLCmd -ServerInstance $source -InputFIle 'C:\temp\gen_full_restore.sql'
+if($1.count -gt 0)
+ {  
 $1.ItemArray[0] | out-file C:\temp\restore_full.sql
+ }
+else { echo "There are no full backups" }
+
 
 $2=Invoke-SQLCmd -ServerInstance $source -InputFIle 'C:\temp\gen_diff_restore.sql'
+if($2.count -gt 0)
+ {  
 $2.ItemArray[0] | out-file C:\temp\restore_diff.sql
-
+}
+else {echo "There are no differential backups"}
 
 $2=Invoke-SQLCmd -ServerInstance $source -InputFIle 'C:\temp\gen_log_restore.sql'
+if ($3.count -gt 0)
+{
 $2.ItemArray[0] | out-file C:\temp\restore_log.sql
+}
+else { echo "There are no log backups" }
 
 
 $configFiles = Get-ChildItem  C:\temp\restore*.sql -rec
@@ -25,4 +37,6 @@ Invoke-Sqlcmd -ServerInstance $target -InputFile C:\temp\restore_full.sql
 
 
 Invoke-Sqlcmd -ServerInstance $target -InputFile C:\temp\restore_diff.sql
+
+
 
